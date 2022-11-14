89d27570b354657d879f1c5a1935dca953dad566
imjoo0 <im_joo0@naver.com>
2022-11-14 16:59:46 +0900

    first commit

---
+++ create.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WEB</title>
+</head>
+<body>
+    <a href="create.php">create</a>
+    <h1>
+        WEB
+    </h1>
+    <form action="process_create.php" method="POST">
+        <p><input type="text" name="title" placeholder="title">제목 </p>
+        <p><textarea name="description" placeholder="본문"></textarea></p>
+        <p> <input type="submit">제출</p>
+    </form>
+</body>
+</html>
+
+
+MYSQLI
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WEB</title>
+</head>
+<body>
+    <a href="create.php">create</a>
+<h1>WEB</h1>
+<ol>
+<li>HTML</li>
+</ol>
+<form action="process_create" title="mysql "dsec+?>
+    <p>
+
+    </p>
+</form>
+<h2>Welcome</h2>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit
+</body>
+</html>
+
+
+MYSQLI
